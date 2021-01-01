a158fe58889c9563b727c3b9edebbea562866605
Vinícius Martins <viniciusmv91@gmail.com>
2021-01-01 20:26:30 -0300

    implemented Pawn

---
+++ ChessConsole/ChessLayer/Pawn.cs
@@ -0,0 +1,78 @@
+﻿using ChessConsole.BoardLayer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessConsole.ChessLayer
+{
+    internal class Pawn : Piece
+    {
+        public Pawn(Board board, Color color) : base(board, color)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "P";
+        }
+
+        private bool IsFree(Position position)
+        {
+            Piece piece = Board.GetPiece(position);
+
+            return piece == null;
+        }
+
+        private bool IsThereAnEnemy(Position position)
+        {
+            Piece piece = Board.GetPiece(position);
+
+            return piece != null && piece.Color != base.Color;
+        }
+
+        public override bool[,] PossibleMoves()
+        {
+            var possibleMovesMat = new bool[Board.Rows, Board.Columns];
+            var position = new Position(0, 0);
+
+            int signal = -1;    // incremnent signal
+            if (base.Color == Color.Black)
+            {
+                signal = +1;
+            }
+
+            // check simple move 'forward'
+            position.SetValues(base.Position.Row + 1 * signal, base.Position.Column);
+            if (Board.IsValidPosition(position) && IsFree(position))
+            {
+                possibleMovesMat[position.Row, position.Column] = true;
+            }
+
+            // check double move 'forward' (only first move)
+            position.SetValues(base.Position.Row + 2 * signal, base.Position.Column);
+            if(Board.IsValidPosition(position) && IsFree(position) && base.TotalMoves == 0)
+            {
+                possibleMovesMat[position.Row, position.Column] = true;
+            }
+
+            // check left diagonal move (attack opponent)
+            position.SetValues(base.Position.Row + 1 * signal, base.Position.Column - 1);
+            if(Board.IsValidPosition(position) && IsThereAnEnemy(position))
+            {
+                possibleMovesMat[position.Row, position.Column] = true;
+            }
+
+            // check right diagonal move (attack opponent)
+            position.SetValues(base.Position.Row + 1 * signal, base.Position.Column + 1);
+            if (Board.IsValidPosition(position) && IsThereAnEnemy(position))
+            {
+                possibleMovesMat[position.Row, position.Column] = true;
+            }
+
+            return possibleMovesMat;
+        }
+    }
+
+}
